d94b9ea3ea6e5c0d0c6827486caa0190234888df
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-28 17:19:52 -0300

    feat: generate dates from start of year

--- web/src/utils/index.ts
+++ web/src/utils/index.ts
@@ -1,3 +1,17 @@
+import { eachDayOfInterval, startOfToday, startOfYear } from "date-fns";
+
 export function cn(...args: Array<string | boolean | undefined>) {
   return args.filter(Boolean).join(' ');
+}
+
+export function generateDatesFromYearBeginning() {
+  const firstDayOfTheYear = startOfYear(new Date());
+  const today = startOfToday();
+
+  const result = eachDayOfInterval({
+    start: firstDayOfTheYear,
+    end: today
+  });
+
+  return result;
 }
